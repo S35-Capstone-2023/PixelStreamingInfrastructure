6639695b5683f9c015751d503cc7c9aa0e2a266c
99wanglin <wanglinsim@gmail.com>
2024-02-14 16:04:09 +0800

    update webpack

--- Frontend/implementations/react/webpack.dev.js
+++ Frontend/implementations/react/webpack.dev.js
@@ -14,20 +14,20 @@ module.exports = merge(common, {
         new webpack.DefinePlugin({
             
             // Refer cloudformation output : Client ID for Cognito (CognitoClientID)
-            'process.env.client_id_cog': JSON.stringify('6l3hobctnis5cgb74foq3vvqd1'),
+            'process.env.client_id_cog': JSON.stringify('7njlcmm9073t0ugu180hjcbctt'),
             // Refer cloudformation output : Domain URL for Cognito (CognitoDomainURL)
             'process.env.cognito_domain': JSON.stringify('https://unreal.auth.ap-southeast-1.amazoncognito.com'),
             // This information is not available in CloudFromation. Please navigate to AWS Console->Cognito. Click on user pool 'ueauthenticationpool'
             // Click on App Integration tab and scroll down to App client list. Click on App Client Name
             // You should be able to retrieve the client secret from the App client information widget by toggling the
             // show client secret button
-            'process.env.client_secret_cog': JSON.stringify('r6mgm18e0n7plitnrn4smmb477u4013vm5vs5cfuoo5oacgmm3d'),
+            'process.env.client_secret_cog': JSON.stringify('bck6hge73b9n1clso8af02k45irsv24jk6fvn625m1dvga4g1cm'),
             // Refer cloudformation output : Callback URL for Cognito (CognitoCallBackURL)
-            'process.env.callback_uri_cog': JSON.stringify('https://d3f8xlzqrtoe0c.cloudfront.net'),
+            'process.env.callback_uri_cog': JSON.stringify('https://d1xm7vtc5cw951.cloudfront.net'),
             // Refer cloudformation output : Web socket endpoint for api server (APIGatewayWSAPI)
-            'process.env.api_ws': JSON.stringify('wss://dd542xnrl9.execute-api.ap-southeast-1.amazonaws.com/production?tokenId=abcd'),
+            'process.env.api_ws': JSON.stringify('wss://x9xvvvgsmh.execute-api.ap-southeast-1.amazonaws.com/production?tokenId=abcd'),
             // Refer cloudformation output : Web socket endpoint for signalling server (SignallingServerWSAPI)
-            'process.env.sig_ws': JSON.stringify('wss://signalling-1052111075.ap-southeast-1.elb.amazonaws.com?'),
+            'process.env.sig_ws': JSON.stringify('wss://signalling-1345965209.ap-southeast-1.elb.amazonaws.com?'),
             // No need to change this. If you modify, make sure you also update clientSecret environment variable for requestSession lambda
             'process.env.sec_token': JSON.stringify('somethingsecret'),
             
