790d579dad923cc605425476e30ad74f23b13ead
99wanglin <wanglinsim@gmail.com>
2024-02-14 16:07:57 +0800

    update startFE.sh

--- Frontend/startFE.sh
+++ Frontend/startFE.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-cd /usr/local/PixelStreamingInfrastructure/Frontend/implementations/react
+cd /usr/local/PixelStreamingInfrastructure/Frontend
+
+cd /library
+
+npm i
+npm run build-dev
+
+cd ../ui-library
+npm i
+npm link ../library
+npm run build-dev
+
+cd ../implementations/react
+npm i
+npm link ../../library ../../ui-library
 
 npm run serve
