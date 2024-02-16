0a196efacc3b35186054965a9a775c897bd92eb9
99wanglin <wanglinsim@gmail.com>
2024-02-17 00:21:31 +0800

    update server.js

--- WebSocket/server.js
+++ WebSocket/server.js
@@ -17,7 +17,7 @@ wss.on('connection', function connection(ws) {
     console.log(`Received data: ${json}`);
     wss.clients.forEach(function each(client) {
         if (client !== ws && client.readyState === WebSocket.OPEN) {
-            client.send(JSON.stringify(json));
+            client.send(json);
         }
         return;
     })
