33162279ce496eb9e7c62eb67d6a6335c2a93a1c
99wanglin <wanglinsim@gmail.com>
2024-02-19 08:39:31 +0800

    update testclient

--- WebSocket/testclient1.js
+++ WebSocket/testclient1.js
@@ -5,9 +5,7 @@ const ws = new WebSocket('ws://localhost:6969');
 
 ws.on('error', console.error);
 
-while (true) {
-  
-setTimeout(()=> axios.get('http://localhost:90/signallingserver').then((res) => {
+axios.get('http://localhost:90/signallingserver').then((res) => {
   if (res.status==200) {
     console.log(res.data);
     url = res.data.signallingServer;
@@ -16,10 +14,8 @@ setTimeout(()=> axios.get('http://localhost:90/signallingserver').then((res) => 
 }).catch((error) => {
   console.log("error block");
   ws.send(JSON.stringify({status: "wait"}));
-}),1000)
-}
-
-
+}).finally(() => {
+});
 
 // ws.on('open', function open() {
 //   ws.send(JSON.stringify({signallingServer: "ws://localhost:80"}));
